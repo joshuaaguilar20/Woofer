e259d2fdb752bfabaec81785b22c8e5ad7034f8c
Joshua Aguilar <joshuaaguilar20@gmail.com>
2019-02-26 15:53:13 -0800

    removed provider
    Removed Provider from app file.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Cardog from './components/Cardog';
-import { Provider } from 'react-redux';
-import { createStore , applyMiddleware } from 'redux';
 
-const store = createStore(() => [],{},applyMiddleware())
+
+
 class App extends Component {
   render() {
     return (
-      <Provider store ={store}>
-      
       <div className="App">
         <Navbar />
         <header className="App-header">
@@ -19,7 +16,6 @@ class App extends Component {
           <Cardog />
         </div>
       </div>
-      </Provider>
     );
   }
 }
