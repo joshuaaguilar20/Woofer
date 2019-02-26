ad991345fe540e91b81ce22833f64520e710b04d
Joshua Aguilar <joshuaaguilar20@gmail.com>
2019-02-26 15:52:12 -0800

    Update index.js
    Fixing style.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//debugger so you can see whats happening with the state. 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers,  composeEnhancers(applyMiddleware(reduxThunk)))
+
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
 
 
